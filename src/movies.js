e4e34d4994e0398d15c97b338b260ada4330cbc6
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-10 12:44:07 +0200

    Iteration2 complete

--- src/movies.js
+++ src/movies.js
@@ -10,7 +10,15 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) { }
+function howManyMovies(moviesArray) {
+    let stevenSpielberg = moviesArray.filter((movie) => {
+        if (movie.director === "Steven Spielberg" && movie.genre.includes("Drama")) {
+            return movie
+        }
+    });
+
+    return stevenSpielberg.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(moviesArray) { }
